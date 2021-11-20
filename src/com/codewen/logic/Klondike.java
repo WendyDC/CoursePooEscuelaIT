844a788e38b68bbdc53fea6fa5e65a5c96b4dbd2
Wendy <carrascalwendy@gmail.com>
2021-11-20 15:57:04 -0500

    Initial

---
+++ src/com/codewen/logic/Klondike.java
@@ -0,0 +1,12 @@
+package com.codewen.logic;
+
+public class Klondike {
+	
+	public static void main(String[] args) {
+		new Klondike().jugar();
+	}
+
+	private void jugar() {
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module CoursePooEscuelaIT {
+}
